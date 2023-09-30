b5ac714bf9cd34a3c0e06edf09e99e856e903717
tienhg1310 <tienhg201@gmail.com>
2023-09-30 17:53:24 +0700

    test connect mysql

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configVewEngineAndStaticFile = require('./config/viewEngineAndStaticFile');
 const webRoute = require('./routes/web');
-// console.log(process.env);
+const mysql = require('mysql2');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -13,6 +13,21 @@ configVewEngineAndStaticFile(app);
 
 // khai bao route
 app.use('/', webRoute);
+
+// test connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  database: 'tienhg2001',
+  user: 'root',
+  password: 'tienhg2001',
+});
+
+// simple query
+connection.query('SELECT * FROM Users u', function (err, results, fields) {
+  console.log('>>> result', results); // results contains rows returned by server
+  console.log('>>> fields', fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
