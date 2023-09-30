f29a287c7e9c4293fa9c155651029821b333e8bc
tienhg1310 <tienhg201@gmail.com>
2023-09-30 22:10:05 +0700

    add env to connect db

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configVewEngineAndStaticFile = require('./config/viewEngineAndStaticFile');
 const webRoute = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database.js');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -15,18 +15,12 @@ configVewEngineAndStaticFile(app);
 app.use('/', webRoute);
 
 // test connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  database: 'tienhg2001',
-  user: 'root',
-  password: 'tienhg2001',
-});
 
 // simple query
 connection.query('SELECT * FROM Users u', function (err, results, fields) {
   console.log('>>> result', results); // results contains rows returned by server
-  console.log('>>> fields', fields); // fields contains extra meta data about results, if available
+  // console.log('>>> fields', fields);
+  // fields contains extra meta data about results, if available
 });
 
 app.listen(port, hostname, () => {
